e782021cd89a09d4f650c0040ca7e12f8446961f
Andrew Plaza <aplaza@liquidthink.net>
2020-03-23 16:24:52 +0100

    make GenericExtrinsic a public type

--- core/src/decoder.rs
+++ core/src/decoder.rs
@@ -29,6 +29,7 @@ mod extrinsics;
 #[cfg(test)]
 pub use self::metadata::test_suite;
 pub use self::metadata::{Metadata, MetadataError, ModuleIndex};
+pub use self::extrinsic::GenericExtrinsic;
 use crate::{
     error::Error,
     substrate_types::{self, StructField, StructUnitOrTuple, SubstrateType},
