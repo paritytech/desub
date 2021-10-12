4b80064b57f44845d1ae9db12841b84c702dc24d
James Wilson <james@jsdw.me>
2021-10-12 17:13:50 +0100

    Add copyright notice on v14 code

--- core_v14/src/metadata/mod.rs
+++ core_v14/src/metadata/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2019 Parity Technologies (UK) Ltd.
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
 // This file is part of substrate-desub.
 //
 // substrate-desub is free software: you can redistribute it and/or modify
@@ -13,9 +13,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with substrate-desub.  If not, see <http://www.gnu.org/licenses/>.
-
-// taken directly and modified from substrate-subxt:
-// https://github.com/paritytech/substrate-subxt
 
 mod version_14;
 
